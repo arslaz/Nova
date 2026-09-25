6185b0dc71162de84e923c2dd9be7bb83ebaeab9
arslaz <7965118arsen@gmail.com>
2026-09-25 22:00:25 +0300

    feat: Camera zoom input box

--- app/src/main/java/com/example/nova/ui/map/MapScreen.kt
+++ app/src/main/java/com/example/nova/ui/map/MapScreen.kt
@@ -1,16 +1,21 @@
 package com.example.nova.ui.map
 
+import android.text.Layout
+import android.util.Log
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
@@ -35,7 +40,13 @@ import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconToggleButton
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.text.style.LineHeightStyle
+import kotlinx.coroutines.launch
 import org.maplibre.compose.camera.CameraUpdate
+import org.maplibre.compose.expressions.dsl.zoom
 import org.maplibre.compose.interaction.ClickResult
 import org.maplibre.compose.interaction.MapInteractions
 import org.maplibre.compose.layers.LocationIndicatorLayer
@@ -46,6 +57,7 @@ import org.maplibre.compose.location.rememberDefaultHeadingProvider
 import org.maplibre.compose.location.rememberDefaultLocationProvider
 import org.maplibre.compose.location.rememberLocationState
 import org.maplibre.compose.map.LocalMapState
+import org.maplibre.compose.map.MapState
 
 @Composable
 fun MyMap(viewModel: MapViewModel = viewModel()) {
@@ -68,8 +80,33 @@ fun MyMap(viewModel: MapViewModel = viewModel()) {
                 provider = locationProvider,
                 headingProvider = headingProvider,
             )
+        val mapState =
+            rememberMapState(
+                baseStyle = BaseStyle.Uri("https://tiles.openfreemap.org/styles/liberty"),
+                initialCameraPosition = CameraPosition(target = Position(latitude = 53.9045, longitude = 27.5615), zoom = 10.0),
+                ){
+                val mapState = checkNotNull(LocalMapState.current)
 
-        MapScreen(viewModel, locationState)
+                LocationIndicatorLayer(
+                    id = "user",
+                    locationState = locationState,
+                )
+                LocationTrackingEffect(locationState = locationState) {
+                    if(viewModel.isFallowing && !viewModel.isFirstFallowing){
+                        mapState.animateCamera(CameraUpdate(target = currentLocation.position, zoom = 15.0))
+                        viewModel.onFirstFallowing()
+                    }else if(viewModel.isFallowing){
+                        mapState.animateCamera(CameraUpdate(target = currentLocation.position))
+                    }
+
+                }
+            }
+
+        MapScreen(viewModel, locationState, mapState)
+        MapZoom(
+            Modifier.align (Alignment.CenterEnd),
+            mapState = mapState,
+        )
 
         if (isMenuOpen) {
             MapInterfaceMenu(
@@ -100,30 +137,12 @@ fun MyMap(viewModel: MapViewModel = viewModel()) {
 }
 
 @Composable
-fun MapScreen(viewModel: MapViewModel = viewModel(), locationState: LocationState){
+fun MapScreen(
+    viewModel: MapViewModel = viewModel(),
+    locationState: LocationState,
+    mapState: MapState
+){
 
-    val mapState =
-        rememberMapState(
-            baseStyle = BaseStyle.Uri("https://tiles.openfreemap.org/styles/liberty"),
-            initialCameraPosition = CameraPosition(target = Position(latitude = 53.9045, longitude = 27.5615), zoom = 10.0),
-
-        ){
-            val mapState = checkNotNull(LocalMapState.current)
-
-            LocationIndicatorLayer(
-                id = "user",
-                locationState = locationState,
-            )
-            LocationTrackingEffect(locationState = locationState) {
-                if(viewModel.isFallowing && !viewModel.isFirstFallowing){
-                    mapState.animateCamera(CameraUpdate(target = currentLocation.position, zoom = 15.0))
-                    viewModel.onFirstFallowing()
-                }else if(viewModel.isFallowing){
-                    mapState.animateCamera(CameraUpdate(target = currentLocation.position))
-                }
-
-            }
-        }
     MaplibreMap (
         modifier = Modifier.fillMaxSize(),
         state = mapState,
@@ -231,4 +250,30 @@ fun MapInterfacePlace(
             tint = Color(0xFFB0BEC5)
         )
     }
+}
+
+@Composable
+fun MapZoom(
+    modifier: Modifier = Modifier,
+    mapState: MapState
+){
+    Box(
+        modifier = modifier
+            .width(width = 40.dp)
+            .clip(CircleShape)
+            .fillMaxHeight()
+            .windowInsetsPadding(WindowInsets.navigationBars)
+            .pointerInput(key1 = Unit) {
+                detectDragGestures(
+                    onDragStart = { startPosition -> },
+                    onDragEnd = { },
+                    onDragCancel = { },
+                ) { change, dragAmount ->
+                    Log.d("zommstrinp", "${dragAmount.y}")
+                    change.consume()
+                    mapState.setCameraPosition(mapState.cameraPosition.copy( zoom = mapState.cameraPosition.zoom - dragAmount.y / 40))
+                }
+            }
+    )
+
 }
